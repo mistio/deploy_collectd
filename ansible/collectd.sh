d4bc572a30bc36762b52273fcb3a1ab38d44032b
Dimitris Rozakis <dimrozakis@mist.io>
2014-09-30 07:52:11 +0300

    Add ansible playbook to deploy collectd
    for mist.io monitoring. It takes 'monitor', 'uuid' and 'password'
    as parameters and runs on either all hosts or those specified
    with 'hosts'.
    
    This is just a draft, tested only in ubuntu and ami vms.

---
+++ ansible/collectd.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+COLLECTD=/usr/sbin/collectd
+CONFIG_FILE=/opt/mistio-collectd/collectd.conf
+PID_FILE=/opt/mistio-collectd/pid
+
+start() {
+  if [ -f $PID_FILE ] && ps -p $(cat $PID_FILE); then
+    echo "mist.io collectd already running"
+    cat $PID_FILE
+    return 1
+  fi
+  echo "Starting mist.io collectd"
+  $COLLECTD -C $CONFIG_FILE -P $PID_FILE
+  cat $PID_FILE
+}
+
+stop() {
+  if [ ! -f $PID_FILE ] || ! ps -p $(cat $PID_FILE); then
+    echo 'mist.io collectd not running'
+    return 1
+  fi
+  echo 'Stopping mist.io collectd'
+  kill $(cat $PID_FILE) && rm -f $PID_FILE
+}
+
+status() {
+  if [ ! -f $PID_FILE ] || ! ps -p $(cat $PID_FILE); then
+    echo "mist.io stopped"
+    return 1
+  fi
+  echo "mist.io collectd running"
+  cat $PID_FILE
+}
+
+case "$1" in
+  start)
+    start
+  ;;
+  stop)
+    stop
+  ;;
+  restart)
+    stop
+    start
+  ;;
+  status)
+    status
+  ;;
+  *)
+    echo "Usage: $0 {start|stop|restart|status}"
+esac
